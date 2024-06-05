fcbcc22c5c8d1f89004197e9dc5be64daaafdd88
Benji <mrbenjaminrussell@icloud.com>
2024-06-05 20:35:10 +0100

    Fixed Base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 site: 'https://sproutgamer.github.io',
-base: 'sproutgamer.github.io',
+base: '/',
 })
